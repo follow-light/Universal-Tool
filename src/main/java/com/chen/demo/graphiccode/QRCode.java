40c801747cc470dfc4575a2b66c1a241b58233c3
follow-light <cz9620@gmail.com>
2022-02-08 10:07:57 +0800

    Update QRCode.java

--- src/main/java/com/chen/demo/graphiccode/QRCode.java
+++ src/main/java/com/chen/demo/graphiccode/QRCode.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 /**
  * @author chenzhuang
  * @create_time 2021/12/22 15:45:43
- * @description
+ * @description 生成二维码
  */
 public class QRCodeGenerater {
     public static void generateCodeImage(String messageInfo, Integer width, Integer hight) throws WriterException, IOException {
