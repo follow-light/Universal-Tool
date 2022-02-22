2ec4ba13427ac96e57266b56ed7b12ed97057564
follow-light <cz9620@gmail.com>
2022-02-22 08:56:19 +0800

    定时器

---
+++ src/main/java/com/chen/demo/timingwheel/Timer.java
@@ -0,0 +1,79 @@
+package com.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 定时器
+ */
+public class Timer {
+
+    /**
+     * 底层时间轮
+     */
+    private TimeWheel timeWheel;
+
+    /**
+     * 一个Timer只有一个delayQueue
+     */
+    private DelayQueue<TimerTaskList> delayQueue = new DelayQueue<>();
+
+    /**
+     * 过期任务执行线程
+     */
+    private ExecutorService workerThreadPool;
+
+    /**
+     * 轮询delayQueue获取过期任务线程
+     */
+    private ExecutorService bossThreadPool;
+
+    /**
+     * 构造函数
+     */
+    public Timer() {
+        timeWheel = new TimeWheel(10000, 30, System.currentTimeMillis(), delayQueue);
+        workerThreadPool = Executors.newFixedThreadPool(10);
+        bossThreadPool = Executors.newFixedThreadPool(1);
+        //20ms获取一次过期任务
+        bossThreadPool.submit(() -> {
+            while (true) {
+                this.advanceClock(1000);
+            }
+        });
+    }
+
+    /**
+     * 添加任务
+     */
+    public void addTask(TimerTask timerTask) {
+        //添加失败任务直接执行
+        if (!timeWheel.addTask(timerTask)) {
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String sd = sdf.format(new Date(timerTask.getDelayMs()));
+            System.out.println("---------任务id：" + timerTask.getIndex() + "-----执行时间：" + sd);
+            workerThreadPool.submit(timerTask.getTask());
+        }
+    }
+
+    /**
+     * 获取过期任务
+     */
+    private void advanceClock(long timeout) {
+        try {
+            TimerTaskList timerTaskList = delayQueue.poll(timeout, TimeUnit.MILLISECONDS);
+            if (timerTaskList != null) {
+                //推进时间
+                timeWheel.advanceClock(timerTaskList.getExpiration());
+                //执行过期任务（包含降级操作）
+                timerTaskList.flush(this::addTask);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
